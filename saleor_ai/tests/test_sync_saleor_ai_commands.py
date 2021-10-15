dfd3f2de62d4e7f16fa9912ca6127fb3ea040d7a
anhht5 <anhht5@smartosc.com>
2021-10-15 15:01:27 +0700

    refactor code

--- saleor_ai/tests/test_sync_saleor_ai_commands.py
+++ saleor_ai/tests/test_sync_saleor_ai_commands.py
@@ -11,20 +11,26 @@ def test_sync_saleor_ai_db():
     # give
     group_saleor_ai = OrderedDict()
     saleor_ai = SaleorAI.objects.values(
-        "article_code", "franchise_code", "min_qty", "max_qty", "start_of_week",
-        "product_class_qty", "product_class_value", "product_class_default"
+        "article_code",
+        "franchise_code",
+        "min_qty",
+        "max_qty",
+        "start_of_week",
+        "product_class_qty",
+        "product_class_value",
+        "product_class_default",
     ).all()
     for item in saleor_ai:
         group_data = {
-            "min_qty": item['min_qty'],
-            "max_qty": item['max_qty'],
-            "product_class_qty": item['product_class_qty'],
-            "product_class_value": item['product_class_value'],
-            "product_class_recommendation": item['product_class_default'],
-            "created_at": item['start_of_week'],
+            "min_qty": item["min_qty"],
+            "max_qty": item["max_qty"],
+            "product_class_qty": item["product_class_qty"],
+            "product_class_value": item["product_class_value"],
+            "product_class_recommendation": item["product_class_default"],
+            "created_at": item["start_of_week"],
         }
         group_saleor_ai.setdefault(
-            (item['article_code'], item['franchise_code']), list()
+            (item["article_code"], item["franchise_code"]), list()
         ).append(group_data)
     # when
     call_command("sync_db_ai")
